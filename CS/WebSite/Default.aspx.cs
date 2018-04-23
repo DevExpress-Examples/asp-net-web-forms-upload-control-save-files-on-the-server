9a6431364c8a08c4676f6b049eca5a7660f73e1f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using DevExpress.Web.ASPxUploadControl;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e){
+
+    }
+
+    protected void ASPxUploadControl1_FilesUploadComplete(object sender, DevExpress.Web.ASPxUploadControl.FilesUploadCompleteEventArgs e){
+        // Intentionally pauses server-side processing to demonstrate the Loading Panel or Progress Panel functionality
+        System.Threading.Thread.Sleep(2000);
+
+        ASPxUploadControl uploadControl = sender as ASPxUploadControl;
+
+        if (uploadControl.UploadedFiles != null && uploadControl.UploadedFiles.Length > 0){
+            for (int i = 0; i < uploadControl.UploadedFiles.Length; i++){
+                UploadedFile file = uploadControl.UploadedFiles[i];
+                if (file.FileName != ""){
+                    string fileName = string.Format("{0}{1}", MapPath("~/Images/"), file.FileName);
+                    //file.SaveAs(fileName, true);//OnLine Demo Restriction
+                }
+            }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to save uploaded files by handling the FilesUploadComplete server event
+
+
+<p>This sample demonstrates how files selected within the ASPxUploadControl can be uploaded via either postback or callback, and how the uploaded files can be saved on the server side by using the FilesUploadComplete event.</p>
+
+<br/>
+
+
